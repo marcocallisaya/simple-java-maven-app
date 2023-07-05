3761ae53886e437054bd8cb889d0a80cdb3f4edc
Marco Callisaya <marcocallisaya20@gmail.com>
2023-07-05 16:42:26 -0400

    update build script

---
+++ .history/scripts/build/build_20230705163747.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Copy the new jar to the build location
+cp -f marco-pipeline/target/*.jar scripts/build/
+
+echo "****************************"
+echo "** Building Docker Image ***"
+echo "****************************"
+
+cd scripts/build/ && docker-compose -f docker-compose-build.yml build --no-cache
